7274cfb1ce62c53a0128582c8b1cd387d7423b0d
Noah Schwartz <noah.amir.schwartz@gmail.com>
2021-02-17 18:42:26 -0500

    Can now remove books from library

--- script.js
+++ script.js
@@ -26,11 +26,17 @@ function addBookToLibrary(){
     let pages = form.elements["pages"].value;
     let completed =form.elements["completed"].checked == true ? "green" : "red";
     myLibrary.push(new Book(author, title, pages, completed, bookNumber));
-    let row = table.insertRow(numOfBooks);
+    let row = table.insertRow(bookNumber);
+    let removeButton = document.createElement("button");
+    removeButton.value = bookNumber;
+    removeButton.innerText = "Remove";
+    addRemoveListener(removeButton);
     row.insertCell(0).textContent = title;
     row.insertCell(1).textContent = author;
     row.insertCell(2).textContent = pages;
-    row.insertCell(3).style.background = completed;
+    row.insertCell(3).style.background = completed
+    row.insertCell(4).appendChild(removeButton);
+    numOfBooks++;
     closeForm();
 }
 
@@ -40,6 +46,18 @@ function openForm(){
     document.getElementById("myForm").style.display = "flex";
     newBookBtn.style.display = "none";
 }
+
+function addRemoveListener(btn){
+    btn.addEventListener("click", removeBook);
+}
+
+
+function removeBook(){
+    let row = table.deleteRow(this.value);
+    myLibrary.splice(myLibrary[this.value, 1]);
+    numOfBooks--;
+}
+
 
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
